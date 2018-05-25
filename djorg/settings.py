8a3db94a609ccc98afb598509f3893fa7de7ed22
Peter Gray <peter@petergraycreative.com>
2018-05-25 11:51:05 -0500

    Update settings.py

--- djorg/settings.py
+++ djorg/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'djorg.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
